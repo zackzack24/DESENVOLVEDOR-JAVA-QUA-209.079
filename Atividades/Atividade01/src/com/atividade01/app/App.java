45d880c5a06c31cc72d4be149bdffc0871cd424b
zackzack24 <luzisaac24@gmail.com>
2025-08-07 17:43:41 -0300

    Correção atividades 01 e 02

--- Atividades/Atividade01/src/com/atividade01/app/App.java
+++ Atividades/Atividade01/src/com/atividade01/app/App.java
@@ -6,24 +6,60 @@ public class App {
     
     public static void main(String[] args) throws Exception {
         Scanner leia = new Scanner(System.in);
-
-         String nome, opcao;
-         int idade;
+            
+         String nome, opcao, diagnostico;
          double altura, peso, imc;
+         int saida = 0;
+        
+         do {
+            System.out.println("1 Calcular IMC");
+            System.out.println("2 SAIR ");
+            System.out.println("Escolha a opção");
+            opcao = leia.nextLine();
 
-         // menu
-         System.out.println("1 - Verificar meu IMC");
-         System.out.println("2 - Sair do programa");
-         System.out.println("Informe a opção desejada");
-         opcao = leia.nextLine();
+            switch (opcao) {
+                case "1":
+                System.out.println("Informe seu nome");
+                nome = leia.nextLine();
+                System.out.println("Informe seu peso em Kg");
+                peso = leia.nextDouble();
+                System.out.println("Informe sua altura em metros");
+                altura = leia.nextDouble();
 
-         // OPÇÃO 2
-         switch (opcao) {
-            case "2": System.out.println("Até mais!"); 
-                break;   
-         }
+                imc = peso / (altura*altura);
+                System.out.println("O IMC de " + nome + "é" + imc + ".");
+                
+                diagnostico = (imc < 18.5) ? nome + "Está abaixo do peso"
+                : (imc < 25) ? nome + "está abaixo do peso."
+                : (imc < 30) ? nome + "está acima do peso."
+                : (imc < 35) ? nome + "está obesa."
+                : (imc < 40) ? nome + "está com obesidade nível II."
+                : nome + " está quase explodindo" ;
 
-        // declaração de variáveis
+                System.out.println(diagnostico);
+                break;
+
+                case "2":
+                    saida = Integer.parseInt(opcao);
+                    System.out.println("Programa Encerrado"); 
+                    break;
+                default:      
+                System.out.println("Opção Inválida");       
+            }
+
+        }while (opcao != "2");
+
+        leia.close();
+    }
+}
+
+        /*  OPÇÃO 2
+          switch (opcao) {
+             case "2": System.out.println("Até mais!"); 
+                 break;   
+        } */
+
+        /*    declaração de variáveis
         System.out.println("Vamos verificar o seu IMC");
         System.out.println("Informe seu nome: ");
         nome = leia.nextLine(); 
@@ -33,11 +69,11 @@ public class App {
         System.out.println("Informe sua altura: ");
         altura = leia.nextDouble(); 
         System.out.println("Informe seu peso: ");
-        peso = leia.nextDouble();
+        peso = leia.nextDouble(); 
         
         imc = peso/(altura*altura);
 
-        // formula do imc = PESO/ALTURA*ALTURA
+         formula do imc = PESO/ALTURA*ALTURA
         if (peso <= 18.5) {
             System.out.println(nome + " está abaixo do peso");
         }
@@ -57,11 +93,10 @@ public class App {
             System.out.println(nome + " Obesidade Mórbida, Morte em breve...");
         }
         
-        
-
         leia.close();
     }
 }
+*\
 
 /* 
          * Crie um Programa que receba o nome, peso e altura do usuário,
@@ -71,3 +106,4 @@ public class App {
          * baseado na tabela do IMC
          * o programa pode calcular o IMC de vários usuários, e deverá ter a opção de sair
         */
+
