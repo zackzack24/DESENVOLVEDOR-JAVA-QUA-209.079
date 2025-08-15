f9783057edb0f93e9018a004bfa5d33bb795f15b
zackzack24 <luzisaac24@gmail.com>
2025-08-15 17:37:27 -0300

    Calculadora

--- GUI/02.Calculadora/src/com/calculadora/app/App.java
+++ GUI/02.Calculadora/src/com/calculadora/app/App.java
@@ -20,9 +20,42 @@ public class App {
             }
 
             if (opcao == "Somar") {
-
+                JOptionPane.showMessageDialog(
+                null, 
+                "O Resultado da Soma é  " + 
+                calculadora.somar(calculadora.getX(), calculadora.getY()),
+                "Soma",
+                JOptionPane.INFORMATION_MESSAGE
+                );
             }
-            
+            else if (opcao == "Subtrair") {
+                JOptionPane.showMessageDialog(
+                null, 
+                "O Resultado da Subtração é  " + 
+                calculadora.subtrair(calculadora.getX(), calculadora.getY()),
+                "Subtrair",
+                JOptionPane.INFORMATION_MESSAGE
+                );
+                
+            }
+            else if (opcao == "Multiplicar") {
+                JOptionPane.showMessageDialog(
+                null, 
+                "O Resultado da Multiplicação é  " +
+                calculadora.multiplicar(calculadora.getX(), calculadora.getY()), 
+                null, 
+                JOptionPane.INFORMATION_MESSAGE);
+            }
+            if (opcao == "dividir") {
+                JOptionPane.showMessageDialog(
+                null,
+                "O Resultado da Divisão é  " + 
+                calculadora.dividir(calculadora.getX(), calculadora.getY()),
+                null, 
+                0
+                );
+            }
+
         } while (opcao != "Sair");
     }
 }
