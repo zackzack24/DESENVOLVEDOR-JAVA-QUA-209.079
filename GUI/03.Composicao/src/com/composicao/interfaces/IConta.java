3cb5e43860d99822f96191d397239772e2162671
zackzack24 <luzisaac24@gmail.com>
2025-08-19 17:44:04 -0300

    Composição

---
+++ GUI/03.Composicao/src/com/composicao/interfaces/IConta.java
@@ -0,0 +1,7 @@
+package com.composicao.interfaces;
+
+public interface IConta {
+    public String exibirDados();
+    public double fazerDeposito(double valor);
+    public double fazerSaque(double valor);
+}
