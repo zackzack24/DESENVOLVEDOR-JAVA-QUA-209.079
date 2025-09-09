01ac7d9cc4613f61e03f5af15a2d8c3857da3eb8
zackzack24 <luzisaac24@gmail.com>
2025-09-09 17:21:19 -0300

    Aula 09/09

---
+++ spring/helloworld/src/test/java/com/helloworld/app/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
